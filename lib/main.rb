6b7126894c0c3ca7934bc40ad606f5a3392d13b3
dostendite <dostendite@outlook.es>
2023-10-16 00:47:52 +0200

    Add base method to print ragdoll
    The method will then take in the amount
    of lives as an argument to print the hangman
    accordingly

--- lib/main.rb
+++ lib/main.rb
@@ -2,10 +2,6 @@
 
 # Main game file
 
-# When a new game is started, your script should load in the
-# dictionary and randomly select a word between 5 and 12
-# characters long for the secret word.
-
 # Main game class w/ main menu
 class Game
   def initialize
@@ -35,11 +31,11 @@ class Game
   def pick_secret_word
     words_file = File.open('words.txt')
     word_list = words_file.readlines.map(&:chomp)
-  
+
     word_list.each do |word|
       word_list.delete(word) if word.length < 5 || word.length > 12
     end
-  
+
     secret_word = word_list.sample
     puts "The secret word is #{secret_word}"
   end
@@ -59,7 +55,7 @@ class Ragdoll
   end
 
   def print_ragdoll
-    # putss the ragdoll based on the lives left
+    
   end
 
   # Every time the player fails a guess,
--- lib/ragdoll.rb
+++ lib/ragdoll.rb
@@ -1,3 +1,26 @@
 # frozen_string_literal: true
 
 # Placeholder file for Ragdoll class
+
+def print_ragdoll
+  canvas_lines = [
+    '     ____________               ',
+    '     | /       _|_              ',
+    '     |/       /   \             ',
+    '     |        \___/             ',
+    '     |         /|\              ',
+    '     |        / | \             ',
+    '     |       /  |  \            ',
+    '     |         / \              ',
+    '     |        /   \             ',
+    '     |\      /     \            ',
+    '     |_\___________________     ',
+    '                                '
+  ]
+
+  system 'clear'
+
+  canvas_lines.each { |line| puts line }
+end
+
+print_ragdoll
